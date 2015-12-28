5e51c7f5b200c9efc84200ee7d7d56fa83dd653a
Baptiste Becmeur <baptistebecmeur@gmail.com>
2015-12-28 15:17:03 +0100

    Chapter model created and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151228141223) do
+ActiveRecord::Schema.define(version: 20151228141517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chapters", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "course_id"
+    t.integer  "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "chapters", ["course_id"], name: "index_chapters_on_course_id", using: :btree
 
   create_table "courses", force: :cascade do |t|
     t.string   "title"
@@ -55,4 +65,5 @@ ActiveRecord::Schema.define(version: 20151228141223) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "chapters", "courses"
 end
