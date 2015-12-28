61debf003ae882942bf92e151bbf2d8bd0f968e4
Baptiste Becmeur <baptistebecmeur@gmail.com>
2015-12-28 15:22:11 +0100

    Item model created and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151228141517) do
+ActiveRecord::Schema.define(version: 20151228142058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,18 @@ ActiveRecord::Schema.define(version: 20151228141517) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "chapter_id"
+    t.integer  "position"
+    t.integer  "actable_id"
+    t.string   "actable_type"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "items", ["chapter_id"], name: "index_items_on_chapter_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -66,4 +78,5 @@ ActiveRecord::Schema.define(version: 20151228141517) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "chapters", "courses"
+  add_foreign_key "items", "chapters"
 end
