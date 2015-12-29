2f4acddabb42fbdfa582623268ff87fe04c91472
Baptiste Becmeur <baptistebecmeur@gmail.com>
2015-12-29 15:39:09 +0100

    Item Course Chapter methods controller added

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
+  
   def show
+    @course = Course.find(params[:course_id])
+    @chapter = @course.chapters.find(params[:chapter_id])
+    @item = @chapter.items.find(params[:id])
   end
 end
