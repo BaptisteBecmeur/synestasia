afc617d07b5005d4f9b0928699ab4f375126d75f
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-01-03 23:25:23 +0100

    link footer + date event added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'start', to: 'pages#start'
   get 'contact', to: 'pages#contact'
+  get 'legal', to: 'pages#legal'
 
   root to: 'pages#home'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
