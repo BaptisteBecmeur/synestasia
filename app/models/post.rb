ed3552869d14d785912c7a022b23e533a6610474
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-01-27 20:22:58 +0100

    show done + css in progress

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,11 @@ class Post < ActiveRecord::Base
   CATEGORIES = ["Design", "Mode", "Tendance", "Life-Style", "Tradition", "Gastronomie", "Insolite", "Technologie"]
 
   validates :category, inclusion: { in: Post::CATEGORIES, allow_nil: false }
-
   validates :user_id, presence: true
   validates :title, presence: true, uniqueness: true
   validates :content, presence: true
+
+  # validates :tag, length: { minimum: 5 }
 
   mount_uploader :cover, ImageUploader
 
