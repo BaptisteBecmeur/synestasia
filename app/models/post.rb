cd9634f1fbdebe3664cd133f53644c65e117db13
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-01-29 12:56:21 +0100

    remove author column to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,16 @@
 class Post < ActiveRecord::Base
   belongs_to :user
 
-  CATEGORIES = ["Design", "Mode", "Tendance", "Life-Style", "Tradition", "Gastronomie", "Insolite", "Technologie"]
+  CATEGORIES = ["Japon", "Chine", "Corée du Sud", "Moyen-Orient"]
+  TAGS = ["Design", "Mode", "Tendance", "Life-Style", "Tradition", "Gastronomie", "Insolite", "Technologie"]
 
+  validates :tag, inclusion: { in: Post::TAGS, allow_nil: false }
   validates :category, inclusion: { in: Post::CATEGORIES, allow_nil: false }
   validates :user_id, presence: true
   validates :title, presence: true, uniqueness: true
   validates :content, presence: true
 
-  # validates :tag, length: { minimum: 5 }
+
 
   mount_uploader :cover, ImageUploader
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160129105653) do
+ActiveRecord::Schema.define(version: 20160129115056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,7 +83,6 @@ ActiveRecord::Schema.define(version: 20160129105653) do
     t.integer  "user_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.string   "author"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
