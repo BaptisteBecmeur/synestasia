77bd2e72dfb13aa9c65ff544003b0d3da37171a6
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-01-31 13:47:02 +0100

    facebook auth done again

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
 
   has_many :posts, dependent: :destroy
 
-    def self.find_for_facebook_oauth(auth)
+   def self.find_for_facebook_oauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
