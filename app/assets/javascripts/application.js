0acf3106500e1ec7febee0b3e2c68c672bdb1464
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-02-09 21:48:23 +0100

    ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,19 +6,19 @@
 
 var simplemde = new SimpleMDE({ spellChecker: false, element: $("#post_content")[0] });
 
-// $ (document).ready(function() {
-//    $('#id faved-on').click(function() {
-//     var fav = $('#id faved-off')
+$ (document).ready(function() {
+   $('.faved-on').click(function() {
+    var fav = $('.faved-off')
 
-//     $.ajax({
-//       type: "GET",
-//       url: "https://api.github.com/users/lewagon/repos",
-//         success: function(data) {
-//       console.log(data);
-//       },
-//       error: function(jqXHR) {
-//       console.error(jqXHR.responseText);
-//       }
-//     });
-//   })
-// })
+    $.ajax({
+      type: "GET",
+      url: "https://synestasia.herokuapp.com/hiraganas",
+        success: function(data) {
+      console.log(data);
+      },
+      error: function(jqXHR) {
+      console.error(jqXHR.responseText);
+      }
+    });
+  })
+})
