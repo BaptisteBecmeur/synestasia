18db06d4b81dee3824dc56461cf63a1ede8b7be3
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-02-09 17:53:35 +0100

    try to add ajax function for fav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,4 +6,19 @@
 
 var simplemde = new SimpleMDE({ spellChecker: false, element: $("#post_content")[0] });
 
+// $ (document).ready(function() {
+//    $('#id faved-on').click(function() {
+//     var fav = $('#id faved-off')
 
+//     $.ajax({
+//       type: "GET",
+//       url: "https://api.github.com/users/lewagon/repos",
+//         success: function(data) {
+//       console.log(data);
+//       },
+//       error: function(jqXHR) {
+//       console.error(jqXHR.responseText);
+//       }
+//     });
+//   })
+// })
