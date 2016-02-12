0b28f9fe5b3569685d9c5f9eced29cc684358c34
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-02-12 15:02:23 +0100

    ajax favs icluded

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,25 +4,8 @@
 //= require simplemde
 //= require_tree .
 
-//var simplemde = new SimpleMDE({ spellChecker: false, element: $("#post_content")[0] });
 
 
-$(document).ready(function() {
-     $('body').on('click', ".faved", function(e) { //make sure to pass in the e (the event paramter)
-     e.preventDefault(); //this is the line you are missing
-     var fav = $('.faved-off')
-     //let your ajax handle the rest then
-     $.ajax({
-       type: "POST",
-       url: "favs/"+this.id,
-       success: function(data) {
-        console.log(data);
-             //change the color of your heart to red here
-           },
-           error: function(jqXHR) {
-            console.error(jqXHR.responseText);
-          }
-        });
-      });
-    });
+
+
 
--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -2,3 +2,4 @@
 // All this logic will automatically be available in application.js.
 
 
+var simplemde = new SimpleMDE({ spellChecker: false, element: $("#post_content")[0] });
