2dd6eab2a21ab8ce0b9ca7e9181f905e56a5c3ce
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-02-14 15:17:19 +0100

    subtitle migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160207154852) do
+ActiveRecord::Schema.define(version: 20160214141556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20160207154852) do
     t.integer  "user_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "subtitle"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
