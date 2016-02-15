9fdd720eb7f2d88b18c5ce497c55395b3e91ae4a
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-02-15 15:14:11 +0100

    new subtitle field doesn't work

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,10 @@ class Post < ActiveRecord::Base
   CATEGORIES = ["Japon", "Chine", "Corée du Sud", "Moyen-Orient"]
   TAGS = ["Design", "Mode", "Tendance", "Life-Style", "Tradition", "Gastronomie", "Insolite", "Technologie"]
 
+  validates :title, presence: true
   validates :tag, inclusion: { in: Post::TAGS, allow_nil: false }
   validates :category, inclusion: { in: Post::CATEGORIES, allow_nil: false }
   validates :user_id, presence: true
-  validates :title, presence: true
   validates :content, presence: true
 
   mount_uploader :cover, ImageUploader
