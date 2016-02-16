6d229d1e2968e19746ebb139db1dbdfd9c6f388e
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-02-16 16:04:03 +0100

    test google

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:facebook, :google, :google_oauth2]
+         :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   has_many :posts, dependent: :destroy
   has_many :hiraganas, dependent: :destroy
