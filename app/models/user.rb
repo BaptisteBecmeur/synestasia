6d5be923850e45ffdf504ac9065d8078c8960610
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-02-17 14:32:56 +0100

    google + à suivre

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+         :omniauthable, omniauth_providers: [:facebook] #, :google_oauth2
 
   has_and_belongs_to_many :oauth_credentials
 
@@ -29,21 +29,6 @@ class User < ActiveRecord::Base
   def admin?
     ["bat@synestasia.co", "aurelie@synestasia.co"].include? self.email
   end
-
-  # #Google
-  #  def self.find_for_google_oauth2(auth)
-  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  #     user.provider = auth.provider
-  #     user.uid = auth.uid
-  #     user.email = auth.info.email
-  #     user.password = Devise.friendly_token[0,20]  # Fake password for validation
-  #     user.first_name = auth.info.first_name
-  #     user.last_name = auth.info.last_name
-  #     user.picture = auth.info.image
-  #     user.token = auth.credentials.token
-  #     user.token_expiry = Time.at(auth.credentials.expires_at)
-  #   end
-  # end
 
   def self.find_for_google_oauth2(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
