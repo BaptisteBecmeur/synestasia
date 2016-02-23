624df68de09ffd68be7cd73b493d88b858209465
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-02-23 15:29:38 +0100

    sound field added to Hiragana

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218133153) do
+ActiveRecord::Schema.define(version: 20160223142858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 20160218133153) do
     t.integer  "user_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "sound"
   end
 
   add_index "hiraganas", ["user_id"], name: "index_hiraganas_on_user_id", using: :btree
