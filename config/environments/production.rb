df8b31ef1953a5cb2b516b0c5f055a499e025b11
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-03-26 11:45:19 +0100

    config action mailer changer as www.synestasia.co

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://synestasia-v1.herokuapp.com/", port: 80 }
+  config.action_mailer.default_url_options = { host: "http://www.synestasia.co", port: 80 }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
